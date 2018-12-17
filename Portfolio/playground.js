bcac45a3f0c38e51b150d13be26c509c3fb2f877
mariusz1389 <43520252+mariusz1389@users.noreply.github.com>
2018-12-17 21:46:00 +0100

    Add files via upload

---
+++ Portfolio/playground.js
@@ -0,0 +1,13 @@
+// console.log("Mariusz Omernik".toUpperCase())
+// console.log('JavaScript jest mocno typowanym językiem'.replace('mocno', 'słabo'))
+// console.log('JavaScript jest językiem wysokiego poziomu'.split)
+// console.log('Jestem królem front-endu! '.repeat(3))
+
+
+console.log((234).toString(2))
+
+console.log(23.2345432.toFixed(3))
+
+console.log(parseFloat('54.34test'))
+
+console.log(isFinite(2e134))
